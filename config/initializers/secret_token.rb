8d976b6155f826b9ddcee930d751be7c8dcd151c
Brandon Sislow <brandons@tailorwell.com>
2013-09-29 14:09:24 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RandomPoints::Application.config.secret_key_base = '647f4738acae392d2ca1d62669aeccc721917d5615b46a7d1cf96a1293ea76dcf29f3124851e5c05f365c8510615a2a7249e5030ea072ba5a98f035f60e9372f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RandomPoints::Application.config.session_store :cookie_store, key: '_random_points_session'
